663d86aad81822f93c99d9f6229439ff0469fbec
ELWChadrock <68598620+ELWChadrock@users.noreply.github.com>
2020-08-20 07:17:34 -0600

    Add files via upload

--- Basic Javascript Projects/JS/Basic_HTML_9.js
+++ Basic Javascript Projects/JS/Basic_HTML_9.js
@@ -31,12 +31,12 @@ function showSlides(n) {
     var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = slides.length = 1}
     if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i< dots.length; i++) {
+    for (i = 0; i< slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    showSlides[slideIndex-1].style.display = "block";
+    slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 }
